82dbdb7b4fee713f104b12575a547f9e023d9b37
SacSLAF <kalpanairosh@gmail.com>
2025-06-07 21:30:54 +0530

    add js data types and log

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
 var data = 'test data';
 const constdata = "constant";
 let age = 34;
-
+const isAuthenticated = true;
+const firstName = null;
+const userData = {
+    name: "John Doe",
+    email: "test@email.com",
+    age: 30,
+};
+const fruits = ["apple", "banana", "cherry"];
+const lastName = undefined; // This will cause an error
+const usersDataSet = [{id:1, name:"Alice"}, {id:2, name:"Bob"},{id:3, name:"Rist"}];
 
 console.log("Hello, World!", {data,constdata,age});
+console.log("Value:", isAuthenticated, "Type:", typeof isAuthenticated);
+console.log("Value:", lastName, "Type:", typeof lastName);
+console.log("Value:", firstName, "Type:", typeof firstName);
+console.log("Value:", fruits, "Type:", typeof fruits);
+console.log("Value:", userData, "Type:", typeof userData);
+console.log("Value:", usersDataSet, "Type:", typeof usersDataSet);
