43c6a548a1ee140db215a5e51977ac8cb2e740ed
SacSLAF <kalpanairosh@gmail.com>
2025-06-14 21:15:32 +0530

    JS data types extended and scopes

--- session-03/script.js
+++ session-03/script.js
@@ -5,7 +5,27 @@
   let a = 10;
   let b = 24;
   const cd = 30;
-  console.log("01", a, b, cd);
+  var dfg = 55;
+  console.log("01", a, b, cd, dfg);
 }
 
-console.log("02", a, b, cd);
+// console.log("02", a, b, cd);
+console.log("03", dfg);
+
+// function scope
+
+function myFunction() {
+  let letData = 100;
+  return letData;
+}
+//This will throw an error because letData is not defined inside the scope. 
+console.log(letData);
+
+
+function addThem(a, b) {
+  return a + b;
+}
+// console.log("04", addThem(10, 20));
+
+let bigNumber = 2342353462463573575375367356735n;
+// console.log("05", typeof bigNumber);
